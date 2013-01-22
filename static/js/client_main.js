99d9d6290d74e85885c4849ff252663992caa97d
manuels <manuel.schoelling@gmx.de>
2013-01-22 20:17:34 +0100

    Fix client side script paths

--- static/js/client_main.js
+++ static/js/client_main.js
@@ -1,19 +1,19 @@
-var root = '../..';
+var root = '../../static/plugins/ep_latex/static/js/';
 
 exports.documentReady = function(hook, context){
   var button = $('#compileLatex');
   var mode = 'compile';
 
-  jQuery.getScript(root+'texlive/website/pdftex.js/release/pdftex-webworker.js');
-  jQuery.getScript(root+'texlive/website/pdftex.js/release/pdftex.js');
-  jQuery.getScript(root+'texlive/website/texlive.js');
+  jQuery.getScript(root+'texlive.js/website/pdftex.js/release/pdftex-webworker.js');
+  jQuery.getScript(root+'texlive.js/website/pdftex.js/release/pdftex.js');
+  jQuery.getScript(root+'texlive.js/website/texlive.js');
 
   var pdf;
   button.click(function() {
     if(mode==='compile') {
-      button.append('<div id="msg">compiling</div>');
+      button.children().append('<div id="msg">compiling</div>');
     
-      var pdftex = new PDFTeX();
+      var pdftex = new PDFTeX(root+'texlive.js/website/');
 
       pdftex.on_stdout = function(txt) { }
       pdftex.on_stderr = function(txt) { }
@@ -25,8 +25,11 @@ exports.documentReady = function(hook, context){
       xhr.open('GET', url, false);
 
       xhr.onreadystatechange = function(ev) {
-        var code = ev.responseText;     
-        texlive.compile(code, root, function(data) {
+        if (xhr.readyState !== 4)
+          return;
+
+        var code = xhr.responseText;
+        texlive.compile(code, '../../', function(data) {
           button.find('#msg').text('click to open');
           pdf = data;
           mode = 'open';
@@ -38,6 +41,7 @@ exports.documentReady = function(hook, context){
     else {
        mode = 'compile';
        window.open('data:application/pdf;base64,'+window.btoa(pdf));
+       button.find('#msg').remove();
     }
   });
 }
